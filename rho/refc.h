74acacd8a9e89499b3fcf6fd7f55b9cdf2e4e53a
Ryan Henning <ryan@rhobota.com>
2012-06-30 14:07:23 -0500

    Refc has been made bullet-proof.

--- rho/refc.h
+++ rho/refc.h
@@ -5,6 +5,7 @@
 #include "ebObject.h"
 
 #include <cstdlib>
+#include <map>
 
 
 namespace rho
@@ -19,11 +20,18 @@ class refc
         refc(T* object);
         refc(const refc& other);
 
-        const refc& operator= (const refc& other);
         const refc& operator= (T* object);
+        const refc& operator= (const refc& other);
 
         T& operator* ();
         T* operator-> ();
+
+        const T& operator* () const;
+        const T* operator-> () const;
+
+        bool operator== (const refc& other) const;
+        bool operator!= (const refc& other) const;
+        bool operator<  (const refc& other) const;
 
         ~refc();
 
@@ -34,6 +42,9 @@ class refc
 };
 
 
+static std::map<void*, int*> gAllKnownRefcObjectsMap;
+
+
 template <class T>
 refc<T>::refc(T* object)
     : m_object(NULL),
@@ -42,8 +53,19 @@ refc<T>::refc(T* object)
     if (object == NULL)
         throw std::logic_error("Reference counting NULL makes no sense.");
 
-    m_object = object;
-    m_ref_count = new int(1);
+    if (gAllKnownRefcObjectsMap.find(object) != gAllKnownRefcObjectsMap.end())
+    {
+        m_object = object;
+        m_ref_count = gAllKnownRefcObjectsMap[object];
+        ++(*m_ref_count);
+    }
+    else
+    {
+        m_object = object;
+        m_ref_count = new int(1);
+        gAllKnownRefcObjectsMap.insert(
+                std::make_pair((void*)m_object, m_ref_count));
+    }
 }
 
 template <class T>
@@ -65,6 +87,7 @@ const refc<T>& refc<T>::operator= (const refc<T>& other)
     --(*m_ref_count);
     if (*m_ref_count == 0)
     {
+        gAllKnownRefcObjectsMap.erase(m_object);
         delete m_object;
         delete m_ref_count;
     }
@@ -88,12 +111,24 @@ const refc<T>& refc<T>::operator= (T* object)
     --(*m_ref_count);
     if (*m_ref_count == 0)
     {
+        gAllKnownRefcObjectsMap.erase(m_object);
         delete m_object;
         delete m_ref_count;
     }
 
-    m_object = object;
-    m_ref_count = new int(1);
+    if (gAllKnownRefcObjectsMap.find(object) != gAllKnownRefcObjectsMap.end())
+    {
+        m_object = object;
+        m_ref_count = gAllKnownRefcObjectsMap[object];
+        ++(*m_ref_count);
+    }
+    else
+    {
+        m_object = object;
+        m_ref_count = new int(1);
+        gAllKnownRefcObjectsMap.insert(
+                std::make_pair((void*)m_object, m_ref_count));
+    }
 
     return *this;
 }
@@ -105,9 +140,39 @@ T& refc<T>::operator* ()
 }
 
 template <class T>
+const T& refc<T>::operator* () const
+{
+    return *m_object;
+}
+
+template <class T>
 T* refc<T>::operator-> ()
 {
     return m_object;
+}
+
+template <class T>
+const T* refc<T>::operator-> () const
+{
+    return m_object;
+}
+
+template <class T>
+bool refc<T>::operator== (const refc<T>& other) const
+{
+    return m_object == other.m_object;
+}
+
+template <class T>
+bool refc<T>::operator!= (const refc<T>& other) const
+{
+    return m_object != other.m_object;
+}
+
+template <class T>
+bool refc<T>::operator< (const refc<T>& other) const
+{
+    return m_object < other.m_object;
 }
 
 template <class T>
@@ -116,6 +181,7 @@ refc<T>::~refc()
     --(*m_ref_count);
     if (*m_ref_count == 0)
     {
+        gAllKnownRefcObjectsMap.erase(m_object);
         delete m_object;
         delete m_ref_count;
     }
--- tests/refcTest.cpp
+++ tests/refcTest.cpp
@@ -17,7 +17,7 @@ using std::vector;
 
 static i64 gObjectCount = 0;
 
-static const i32 kMaxTests = 100000;
+static const i32 kMaxTests = 1000;
 
 
 class tCountingObject
@@ -48,14 +48,19 @@ class tCountingObject
         void foo()
         {
         }
+
+        void bar() const
+        {
+        }
 };
 
 
 void randomTest()
 {
+    // Randomized test 1:
     {
-        const int kVectorSize = 1000;
-        std::vector< refc<tCountingObject> > v;
+        const int kVectorSize = 100;
+        vector< refc<tCountingObject> > v;
         for (int i = 0; i < kVectorSize; i++)
         {
             refc<tCountingObject> r = new tCountingObject;
@@ -70,24 +75,139 @@ void randomTest()
         }
     }
     assert(gObjectCount == 0);
+
+    // Randomized test 2:
+    {
+        const int kVectorSize = 100;
+        vector< refc<tCountingObject> > v;
+        for (int i = 0; i < kVectorSize; i++)
+        {
+            v.push_back(refc<tCountingObject>(new tCountingObject));
+        }
+
+        for (int i = 0; i < 100*kVectorSize; i++)
+        {
+            int pos1 = rand() % kVectorSize;
+            int pos2 = rand() % kVectorSize;
+            int op   = rand() % 6;
+            switch (op)
+            {
+                case 0:
+                {
+                    // Tests operator=(T*) with a new object.
+                    v[pos1] = new tCountingObject;
+                    break;
+                }
+                case 1:
+                {
+                    // Tests operator=(T*) with a per-existing object.
+                    tCountingObject& o = *v[pos2];
+                    v[pos1] = &o;
+                    break;
+                }
+                case 2:
+                {
+                    // Tests refc(T*) and operator=(refc&) with a new object.
+                    refc<tCountingObject> r(new tCountingObject);
+                    v[pos1] = r;
+                    break;
+                }
+                case 3:
+                {
+                    // Tests refc(T*) and operator=(refc&) with a pre-ex object.
+                    tCountingObject& o = *v[pos2];
+                    refc<tCountingObject> r(&o);
+                    v[pos1] = r;
+                    break;
+                }
+                case 4:
+                {
+                    // Tests operator=(refc&).
+                    v[pos1] = v[pos2];
+                    break;
+                }
+                case 5:
+                {
+                    // Tests refc(refc&).
+                    refc<tCountingObject> r1(v[pos1]);
+                    refc<tCountingObject> r2(v[pos2]);
+                    break;
+                }
+                default:
+                {
+                    assert(false);
+                    break;
+                }
+            }
+        }
+    }
+    assert(gObjectCount == 0);
 }
 
 
 int main()
 {
+    // Example:
     {
         refc<int> r = new int(99);
         int x = *r;                  // x == 99
     } // --------------------------> // goes out of scope and is deleted here
 
+    // Example:
     {
         assert(gObjectCount == 0);
         refc<tCountingObject> r = new tCountingObject;
         assert(gObjectCount == 1);
-        r->foo();                    // cool huh?
-    } // --------------------------> // the tCountingObject is deleted here
-    assert(gObjectCount == 0); // -> // proof the delete happened
 
+        tCountingObject& c = *r;
+        r->foo();
+    } // --------------------------> // the tCountingObject is deleted here
+    assert(gObjectCount == 0);
+
+    // Example:
+    {
+        assert(gObjectCount == 0);
+        const refc<tCountingObject> r = new tCountingObject;
+        assert(gObjectCount == 1);
+
+        const tCountingObject& c = *r;
+        r->bar();
+    }
+    assert(gObjectCount == 0);
+
+    // Example:
+    {
+        refc<int> myref = new int(15);
+        refc<int> myref2 = myref;
+        refc<int> myref3 = new int(48);
+
+        assert(myref == myref2);
+        assert(myref != myref3);
+        assert(myref2 != myref3);
+        assert(!(myref < myref2));
+    }
+
+    // Example:
+    {
+        tCountingObject* object = new tCountingObject;
+        refc<tCountingObject> myref(object);
+        assert(gObjectCount == 1);
+
+        {
+            refc<tCountingObject> myref2(new tCountingObject);
+            assert(gObjectCount == 2);
+        }
+        assert(gObjectCount == 1);
+
+        {
+            refc<tCountingObject> myref2(object);
+            assert(gObjectCount == 1);     // because 'object' is already refc'd
+        }
+        assert(gObjectCount == 1);
+    }
+    assert(gObjectCount == 0);
+
+    // Test:
     {
         refc<tCountingObject> myref = new tCountingObject;
         refc<tCountingObject> myref2 = new tCountingObject;
@@ -97,20 +217,34 @@ int main()
     }
     assert(gObjectCount == 0);
 
+    // Test:
     {
         refc<tCountingObject> myref = new tCountingObject;
         refc<tCountingObject> myref2 = myref;
         assert(gObjectCount == 1);
-    }
-    assert(gObjectCount == 0);
-
-    {
-        refc<tCountingObject> myref(new tCountingObject);
-        refc<tCountingObject> myref2(myref);
+        {
+            refc<tCountingObject> myref3 = myref2;
+            assert(gObjectCount == 1);
+        }
         assert(gObjectCount == 1);
     }
     assert(gObjectCount == 0);
 
+    // Test:
+    {
+        refc<tCountingObject> myref(new tCountingObject);
+        refc<tCountingObject> myref2(myref);
+        assert(gObjectCount == 1);
+        {
+            refc<tCountingObject> myref3(myref2);
+            refc<tCountingObject> myref4(myref);
+            assert(gObjectCount == 1);
+        }
+        assert(gObjectCount == 1);
+    }
+    assert(gObjectCount == 0);
+
+    // Many randomized tests:
     srand(time(0));
     for (int i = 0; i < kMaxTests; i++)
     {
