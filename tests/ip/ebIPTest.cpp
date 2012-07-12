3a92af4ed1fb0e25c146cf85e4c6e4f67d9d0fb9
Ryan Henning <ryan@rhobota.com>
2012-07-12 18:44:20 -0500

    Fixed up the tcp tests and added yeild(), msleep(), and usleep() to tThread.

--- tests/ip/ebIPTest.cpp
+++ tests/ip/ebIPTest.cpp
@@ -1,12 +1,32 @@
 #include "rho/ip/ebIP.h"
+#include "rho/tCrashReporter.h"
+#include "rho/tTest.h"
 
 
-/*
- * These are all tested in the tAddrGroup, tcp::tServer, and tcp::tSocket tests.
- */
+using namespace rho;
+
+
+void test(const tTest& t)
+{
+    ip::ebIP e1("bla...");
+    ip::eSocketCreationError e2("bla...");
+    ip::eSocketBindError e3("bla...");
+    ip::eHostNotFoundError e4("bla...");
+    ip::eHostUnreachableError e5("bla...");
+
+    ebObject* p1 = &e1;
+    ip::ebIP* p2 = &e2;
+    ip::ebIP* p3 = &e3;
+    ip::ebIP* p4 = &e4;
+    ip::ebIP* p5 = &e5;
+}
 
 
 int main()
 {
+    tCrashReporter::init();
+
+    tTest("ebIP test", test);
+
     return 0;
 }
