e26ebd09fc79a5e13d82bf94ed66ee7cf0e0bdb2
Ryan Henning <ryan@rhobota.com>
2012-09-26 18:03:05 -0500

    tVector is finished, but I'm going to break it out into a .h and a .cpp later. Right now it is all in a .h.

---
+++ include/rho/geo/units.h
@@ -0,0 +1,21 @@
+#ifndef __rho_geo_units_h__
+#define __rho_geo_units_h__
+
+
+namespace rho
+{
+namespace geo
+{
+
+
+extern const double kPI;
+
+double toRadians(double angle);
+double toDegrees(double angle);
+
+
+}
+}
+
+
+#endif  // __rho_geo_units_h__
