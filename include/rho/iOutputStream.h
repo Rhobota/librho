5f4427981b8097abb8c5e486d64766b9b141c31c
Ryan Henning <ryan@rhobota.com>
2012-11-23 11:25:21 -0600

    readAll() and writeAll() added to the iInputStream and iOutputStream interfaces.

--- include/rho/iOutputStream.h
+++ include/rho/iOutputStream.h
@@ -17,8 +17,24 @@ class iOutputStream
          * Writes from 'buffer' up to 'length' number of bytes.
          * Returns the number of bytes actually written,
          * or -1 if the stream is closed.
+         *
+         * Writes only what will immediately fit into the output
+         * stream's buffer, but blocks if no bytes can be immediately
+         * written.
          */
-        virtual int write(const u8* buffer, int length) = 0;
+        virtual i32 write(const u8* buffer, i32 length) = 0;
+
+        /**
+         * Writes from 'buffer' up to 'length' number of bytes.
+         * Returns the number of bytes actually written,
+         * or -1 if the stream is closed.
+         *
+         * Will write exactly 'length' bytes to the stream,
+         * unless eof is reached or the stream is closed.
+         * That is, it will block until 'length' bytes can
+         * be written.
+         */
+        virtual i32 writeAll(const u8* buffer, i32 length) = 0;
 
         virtual ~iOutputStream() { }
 };
