d512897f14cd2c41a11f38b28de9bd97829af63e
Ryan Henning <ryan@rhobota.com>
2012-12-14 12:32:08 -0600

    Two bug fixes. One in ebObject and one in refc.

--- include/rho/ebObject.h
+++ include/rho/ebObject.h
@@ -27,7 +27,7 @@ class ebObject : public std::exception
 
         void printStacktrace(std::ostream& o) const;
 
-        ~ebObject() throw();
+        virtual ~ebObject() throw();
 
     private:
 
