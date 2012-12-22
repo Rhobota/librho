82188b221da70cdf8633c9ca758de5033290c5aa
Ryan Henning <ryan@rhobota.com>
2012-12-21 22:53:42 -0600

    Small virtual test for iFlushable.

---
+++ tests/rho/iFlushableTest.cpp
@@ -0,0 +1,45 @@
+#include <rho/iFlushable.h>
+#include <rho/tCrashReporter.h>
+#include <rho/tTest.h>
+
+
+using namespace rho;
+
+
+int gItWorked = 0;
+
+
+class tFoo : public iFlushable
+{
+    public:
+
+        void flush()
+        {
+            gItWorked++;
+        }
+
+        ~tFoo()
+        {
+            gItWorked++;
+        }
+};
+
+
+void test(const tTest& t)
+{
+    iFlushable* p = new tFoo();
+    p->flush();                       // makes sure flush() is virtual
+    t.assert(gItWorked == 1);
+    delete p;                         // makes sure the d'tor is virtual
+    t.assert(gItWorked == 2);
+}
+
+
+int main()
+{
+    tCrashReporter::init();
+
+    tTest("iFlushable test", test);
+
+    return 0;
+}
