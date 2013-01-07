ac71e8e1f32f8767b0be5048099410573387b207
Ryan Henning <ryan@rhobota.com>
2013-01-07 01:03:19 -0600

    This should have gone in the previous changelist...

--- tests/RunTests.bash
+++ tests/RunTests.bash
@@ -5,7 +5,7 @@ INCLUDE_DIR="$TEST_DIR/../include"
 LIBRHO_PATH="$TEST_DIR/../objects/librho.a"
 OUT_FILE="a.out"
 
-CC="g++"
+CC="$TARGET""g++"
 CC_FLAGS+=" -O0 -fno-inline -g -rdynamic -Wall -Werror -I $INCLUDE_DIR"
 CC_LIB_FLAGS+="$LIBRHO_PATH"
 
