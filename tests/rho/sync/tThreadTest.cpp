b0bb15f8ec216e9738adadb5d4d8b9285bce0adb
Ryan Henning <ryan@rhobota.com>
2013-01-16 12:40:02 -0600

    Port to Mingw is finished! It was finished in the last commit, actually, but I thought it wasn't because the refc test was crashing. I spent a long time looking into why, and eventually decided it shouldn't be crashing. So I upgraded Mingw and that fixed the problem. The previous version of Mingw was 3.4.5. I upgraded to 4.5.2.

--- tests/rho/sync/tThreadTest.cpp
+++ tests/rho/sync/tThreadTest.cpp
@@ -1,39 +1,86 @@
 #include <rho/sync/tThread.h>
+#include <rho/bNonCopyable.h>
 #include <rho/tCrashReporter.h>
 #include <rho/tTest.h>
 
-#include <iostream>
+#include <vector>
 
 
 using namespace rho;
-using std::cout;
-using std::endl;
 
 
-const int kNumTestIterations = 10000;
+const int kNumSingleThreads = 10000;
+const int kNumConcurrentThreads = 200;
 
-bool gThreadDidRunYay = false;
 
-
-class tFoo : public sync::iRunnable
+class tFoo : public sync::iRunnable, public bNonCopyable
 {
     public:
 
+        tFoo(bool doWork)
+            : m_threadDidRunYay(false), m_doWork(doWork)
+        {
+        }
+
         void run()
         {
-            gThreadDidRunYay = true;
+            if (m_doWork)
+            {
+                int answer = 0;
+                for (int i = 0; i < 10000000; i++)
+                    answer *= (i+4)-i*i;
+            }
+            m_threadDidRunYay = true;
         }
+
+        bool didRun()
+        {
+            return m_threadDidRunYay;
+        }
+
+    private:
+
+        bool m_threadDidRunYay;
+        bool m_doWork;
 };
 
 
 void test1(const tTest& t)
 {
-    gThreadDidRunYay = false;
+    refc<tFoo> foo(new tFoo(false));
+    refc<sync::iRunnable> runnable(foo);
 
-    sync::tThread thread(refc<sync::iRunnable>(new tFoo));
+    sync::tThread thread(runnable);
     thread.join();
 
-    t.iseq(gThreadDidRunYay, true);
+    t.assert(foo->didRun());
+}
+
+
+void test2(const tTest& t)
+{
+    std::vector< refc<tFoo> > foos;
+    std::vector< refc<sync::tThread> > threads;
+
+    for (int i = 0; i < kNumConcurrentThreads; i++)
+    {
+        refc<tFoo> foo(new tFoo(true));
+        foos.push_back(foo);
+
+        refc<sync::tThread> thread(new sync::tThread(refc<sync::iRunnable>(foo)));
+        threads.push_back(thread);
+    }
+
+    for (size_t i = 0; i < threads.size(); i++)
+    {
+        threads[i]->join();
+    }
+
+    t.assert((int)foos.size() == kNumConcurrentThreads);
+    for (size_t i = 0; i < foos.size(); i++)
+    {
+        t.assert(foos[i]->didRun());
+    }
 }
 
 
@@ -41,7 +88,8 @@ int main()
 {
     tCrashReporter::init();
 
-    tTest("tThread test 1", test1, kNumTestIterations);
+    tTest("Single-thread test", test1, kNumSingleThreads);
+    tTest("Concurrent test", test2);
 
     return 0;
 }
