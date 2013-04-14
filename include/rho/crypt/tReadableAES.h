39036ac1eb73f8694ed71c391d30632042a0a179
Ryan Henning <ryan@rhobota.com>
2013-04-14 15:30:28 -0500

    tReadableAES is good, I think.

--- include/rho/crypt/tReadableAES.h
+++ include/rho/crypt/tReadableAES.h
@@ -19,12 +19,28 @@ class tReadableAES : public iReadable
         tReadableAES(iReadable* internalStream,
                      const u8 key[], eKeyLengthAES keylen);
 
+        ~tReadableAES();
+
         i32 read(u8* buffer, i32 length);
         i32 readAll(u8* buffer, i32 length);
 
     private:
 
-        iReadable* m_stream;
+        bool refill();
+
+    private:
+
+        // Stuff:
+        tBufferedReadable m_stream;
+        u8* m_buf;
+        u32 m_bufSize;
+        u32 m_bufUsed;
+        u32 m_pos;
+        u32 m_chunkBytesLeftToRead;
+
+        // Encryption state:
+        u32 m_rk[60];    // size is 4*(MAXNR+1)
+        int m_Nr;
 };
 
 
