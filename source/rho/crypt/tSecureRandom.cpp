2a76db9d85dc6c52dc27c8417449f675af39c6b8
Ryan Henning <ryan@rhobota.com>
2013-07-22 12:27:11 -0500

    Better streams, and also tRSA and tWritableAES now have their own dedicated tSecureRandom objects to use (they used to both call rand(), which is not good at all).

--- source/rho/crypt/tSecureRandom.cpp
+++ source/rho/crypt/tSecureRandom.cpp
@@ -19,15 +19,16 @@ namespace crypt
 tSecureRandom::tSecureRandom()
 {
     #if __linux__ || __APPLE__ || __CYGWIN__
-    FILE* fp = fopen("/dev/urandom", "r");
-    if (!fp)
-        throw eResourceAcquisitionError("Cannot open /dev/urandom");
-    m_internal = fp;
+
+    m_internal = new tBufferedReadable(new tFileReadable("/dev/urandom"));
+
     #elif __MINGW32__
+
     HCRYPTPROV hProvider = NULL;
     if (!::CryptAcquireContextW(&hProvider, 0, 0, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT | CRYPT_SILENT))
         throw eResourceAcquisitionError("Cannot create a secure random thingy on windows!");
     m_internal = hProvider;
+
     #else
     #error What platform are you on!?
     #endif
@@ -38,9 +39,16 @@ tSecureRandom::~tSecureRandom()
     if (m_internal)
     {
         #if __linux__ || __APPLE__ || __CYGWIN__
-        fclose((FILE*)m_internal);
+
+        tBufferedReadable* bufReader = (tBufferedReadable*)m_internal;
+        iReadable* fileReader = bufReader->getInternalStream();
+        delete bufReader;
+        delete fileReader;
+
         #elif __MINGW32__
+
         ::CryptReleaseContext((HCRYPTPROV)m_internal, 0);
+
         #else
         #error What platform are you on!?
         #endif
@@ -51,21 +59,31 @@ tSecureRandom::~tSecureRandom()
 
 i32 tSecureRandom::read(u8* buffer, i32 length)
 {
-    return readAll(buffer, length);
+    #if __linux__ || __APPLE__ || __CYGWIN__
+
+    return ((tBufferedReadable*)m_internal)->read(buffer, length);
+
+    #elif __MINGW32__
+
+    if (!::CryptGenRandom((HCRYPTPROV)m_internal, (DWORD)length, (BYTE*)buffer))
+        throw eResourceAcquisitionError("Cannot generate any secure random bytes on windows!");
+
+    #else
+    #error What platform are you on!?
+    #endif
 }
 
 i32 tSecureRandom::readAll(u8* buffer, i32 length)
 {
-    #if __linux__ || __APPLE__ || __CYGWIN__
-    for (i32 i = 0; i < length; i++)
-        buffer[i] = (u8) fgetc((FILE*)m_internal);
-    return length;
-    #elif __MINGW32__
-    if (!::CryptGenRandom((HCRYPTPROV)m_internal, (DWORD)length, (BYTE*)buffer))
-        throw eResourceAcquisitionError("Cannot generate any secure random bytes on windows!");
-    #else
-    #error What platform are you on!?
-    #endif
+    i32 amountRead = 0;
+    while (amountRead < length)
+    {
+        i32 n = read(buffer+amountRead, length-amountRead);
+        if (n <= 0)
+            return (amountRead>0) ? amountRead : n;
+        amountRead += n;
+    }
+    return amountRead;
 }
 
 
