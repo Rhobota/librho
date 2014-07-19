25ab5749a931c7c9ffb915c3dc968326bbff304c
Ryan Henning <ryan@rhobota.com>
2014-07-19 16:38:22 -0500

    More mingw fixes.

--- source/rho/ip/_pre.h
+++ source/rho/ip/_pre.h
@@ -19,9 +19,12 @@ extern const int kSigPipeIgnoreKickoff;
 
 #elif __MINGW32__
 
-#define  NTDDI_VERSION  NTDDI_WINXPSP1
+#define  NTDDI_VERSION  NTDDI_WINXPSP1   // NTDDI_WINXPSP1 or NTDDI_VISTASP1
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#ifndef IPV6_V6ONLY
+#define IPV6_V6ONLY 27    // IPV6_V6ONLY is missing from some MinGW versions.
+#endif
 
 extern const int kInitForWindowsKickoff;
 
