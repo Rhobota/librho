6a6a634005fb930dccc5fb0e41ada71b274e46fa
Ryan Henning <ryan@rhobota.com>
2014-12-13 14:46:45 -0600

    A speed test.

--- tests/rho/crypt/tReadableAESTest.cpp
+++ tests/rho/crypt/tReadableAESTest.cpp
@@ -1,6 +1,8 @@
 #include <rho/crypt/tReadableAES.h>
+#include <rho/crypt/tWritableAES.h>
 #include <rho/tCrashReporter.h>
 #include <rho/tTest.h>
+#include <rho/sync/tTimer.h>
 
 #include <iostream>
 #include <vector>
@@ -61,6 +63,42 @@ void test256(const tTest& t)
 }
 
 
+void testSpeed128(const tTest& t)
+{
+    u8 key128[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+    t.assert(sizeof(key128) == 128/8);
+
+    tByteWritable bw;
+    crypt::tWritableAES cw(&bw, crypt::kOpModeCBC, key128, crypt::k128bit);
+
+    f64 start = sync::tTimer::usecTime();
+    const i32 kBufSize = 4096;
+    u8 buf[kBufSize];
+    const i32 kTotalSize = 1000000000;
+    for (i32 i = 0; i < kTotalSize; i += kBufSize)
+    {
+        i32 w = cw.writeAll(buf, kBufSize);
+        t.assert(w == kBufSize);
+    }
+    cw.flush();
+    f64 end = sync::tTimer::usecTime();
+    std::cout << (end - start) / 1000000 << std::endl;
+
+    tByteReadable br(bw.getBuf());
+    crypt::tReadableAES cr(&br, crypt::kOpModeCBC, key128, crypt::k128bit);
+
+    start = sync::tTimer::usecTime();
+    while (true)
+    {
+        i32 r = cr.readAll(buf, kBufSize);
+        if (r <= 0)
+            break;
+    }
+    end = sync::tTimer::usecTime();
+    std::cout << (end - start) / 1000000 << std::endl;
+}
+
+
 int main()
 {
     tCrashReporter::init();
@@ -68,6 +106,8 @@ int main()
     tTest("tReadableAES 128bit test", test128);
     tTest("tReadableAES 192bit test", test192);
     tTest("tReadableAES 256bit test", test256);
+
+    //tTest("tReadableAES 128bit speed test", testSpeed128);
 
     return 0;
 }
