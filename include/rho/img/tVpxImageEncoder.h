efb455b11c0c26df5546158d411ab20494b08482
Ryan Henning <ryan@rhobota.com>
2015-01-09 19:36:26 -0600

    The beginning of some stuff that uses VPX for streaming images in a network-efficient way. VPX is a thing owned by Google and patented by them, but licenses are free!

---
+++ include/rho/img/tVpxImageEncoder.h
@@ -0,0 +1,43 @@
+#ifndef __tVpxImageEncoder_h__
+#define __tVpxImageEncoder_h__
+
+
+#include <rho/bNonCopyable.h>
+#include <rho/iPackable.h>
+#include <rho/img/tImage.h>
+
+
+namespace rho
+{
+namespace img
+{
+
+
+class tVpxImageEncoder : public bNonCopyable
+{
+    public:
+
+        tVpxImageEncoder(iWritable* writable,   // <-- the stream to write encoded images to
+                         u32 bitrate,           // <-- in bits per second
+                         u32 fps,               // <-- what to expect the frames-per-second to be
+                         u32 imageWidth,        // <-- what to expect image widths to be
+                         u32 imageHeight);      // <-- what to expect image heights to be
+
+        void encodeImage(const tImage& image);
+
+    private:
+
+        iWritable* m_writable;
+        u32 m_bitrate;
+        u32 m_fps;
+        u32 m_width;
+        u32 m_height;
+        tImage m_tempImage;
+};
+
+
+}  // namespace img
+}  // namespace rho
+
+
+#endif // __tVpxImageEncoder_h__
