7bcb9f21afc5fa3e094273cf51355878f671ae42
Ryan Henning <ryan@rhobota.com>
2015-04-08 00:25:09 -0500

    I think this new way is better, but it's not quite working yet.

--- source/rho/crypt/aesni/iaes_asm_interface.h
+++ source/rho/crypt/aesni/iaes_asm_interface.h
@@ -42,92 +42,43 @@
 typedef unsigned char UCHAR;
 
 
-#if (__cplusplus)
-extern "C"
-{
-#endif
-
 //structure to pass aes processing data to asm level functions
-typedef struct _sAesData
+struct sAesData
 {
-	UCHAR	*in_block;
-	UCHAR	*out_block;
-	UCHAR	*expanded_key;
-	UCHAR	*iv;					// for CBC mode
-	size_t	num_blocks;
-} sAesData;
-
-#if 0
-#define MYSTDCALL __stdcall
-#else
-#define MYSTDCALL
-#endif
-
-#ifdef __linux__
-#ifndef __LP64__
-#define iEncExpandKey256 _iEncExpandKey256
-#define iEncExpandKey192 _iEncExpandKey192
-#define iEncExpandKey128 _iEncExpandKey128
-#define iDecExpandKey256 _iDecExpandKey256
-#define iDecExpandKey192 _iDecExpandKey192
-#define iDecExpandKey128 _iDecExpandKey128
-#define iEnc128 _iEnc128
-#define iDec128 _iDec128
-#define iEnc256 _iEnc256
-#define iDec256 _iDec256
-#define iEnc192 _iEnc192
-#define iDec192 _iDec192
-#define iEnc128_CBC _iEnc128_CBC
-#define iDec128_CBC _iDec128_CBC
-#define iEnc256_CBC _iEnc256_CBC
-#define iDec256_CBC _iDec256_CBC
-#define iEnc192_CBC _iEnc192_CBC
-#define iDec192_CBC _iDec192_CBC
-#define iEnc128_CTR _iEnc128_CTR
-#define iEnc192_CTR _iEnc192_CTR
-#define iEnc256_CTR _iEnc256_CTR
-#define do_rdtsc    _do_rdtsc
-#endif
-#endif
-	// prepearing the different key rounds, for enc/dec in asm
-	// expnaded key should be 16-byte aligned
-	// expanded key should have enough space to hold all key rounds (16 bytes per round) - 256 bytes would cover all cases (AES256 has 14 rounds + 1 xor)
-	void MYSTDCALL iEncExpandKey256(UCHAR *key, UCHAR *expanded_key);
-	void MYSTDCALL iEncExpandKey192(UCHAR *key, UCHAR *expanded_key);
-	void MYSTDCALL iEncExpandKey128(UCHAR *key, UCHAR *expanded_key);
-
-	void MYSTDCALL iDecExpandKey256(UCHAR *key, UCHAR *expanded_key);
-	void MYSTDCALL iDecExpandKey192(UCHAR *key, UCHAR *expanded_key);
-	void MYSTDCALL iDecExpandKey128(UCHAR *key, UCHAR *expanded_key);
+    UCHAR   *in_block;
+    UCHAR   *out_block;
+    UCHAR   *expanded_key;
+    UCHAR   *iv;                    // for CBC mode
+    size_t  num_blocks;
+};
 
 
-	//enc/dec asm functions
-	void MYSTDCALL iEnc128(sAesData *data);
-	void MYSTDCALL iDec128(sAesData *data);
-	void MYSTDCALL iEnc256(sAesData *data);
-	void MYSTDCALL iDec256(sAesData *data);
-	void MYSTDCALL iEnc192(sAesData *data);
-	void MYSTDCALL iDec192(sAesData *data);
+// prepearing the different key rounds, for enc/dec in asm
+// expnaded key should be 16-byte aligned
+// expanded key should have enough space to hold all key rounds (16 bytes per round) - 256 bytes would cover all cases (AES256 has 14 rounds + 1 xor)
+void iEncExpandKey256(UCHAR *key, UCHAR *expanded_key);
+void iEncExpandKey192(UCHAR *key, UCHAR *expanded_key);
+void iEncExpandKey128(UCHAR *key, UCHAR *expanded_key);
 
-	void MYSTDCALL iEnc128_CBC(sAesData *data);
-	void MYSTDCALL iDec128_CBC(sAesData *data);
-	void MYSTDCALL iEnc256_CBC(sAesData *data);
-	void MYSTDCALL iDec256_CBC(sAesData *data);
-	void MYSTDCALL iEnc192_CBC(sAesData *data);
-	void MYSTDCALL iDec192_CBC(sAesData *data);
+void iDecExpandKey256(UCHAR *key, UCHAR *expanded_key);
+void iDecExpandKey192(UCHAR *key, UCHAR *expanded_key);
+void iDecExpandKey128(UCHAR *key, UCHAR *expanded_key);
 
 
-	void MYSTDCALL iEnc128_CTR(sAesData *data);
-	void MYSTDCALL iEnc256_CTR(sAesData *data);
-	void MYSTDCALL iEnc192_CTR(sAesData *data);
+//enc/dec asm functions
+void iEnc128(sAesData *data);
+void iDec128(sAesData *data);
+void iEnc256(sAesData *data);
+void iDec256(sAesData *data);
+void iEnc192(sAesData *data);
+void iDec192(sAesData *data);
 
-	// rdtsc function
-	unsigned long long do_rdtsc(void);
-
-
-#if (__cplusplus)
-}
-#endif
+void iEnc128_CBC(sAesData *data);
+void iDec128_CBC(sAesData *data);
+void iEnc256_CBC(sAesData *data);
+void iDec256_CBC(sAesData *data);
+void iEnc192_CBC(sAesData *data);
+void iDec192_CBC(sAesData *data);
 
 
 #endif
